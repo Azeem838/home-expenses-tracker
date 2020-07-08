965ba15fabd30863e9df0b98f77ae5dd8d738e28
Azeem <azeemahmed101@gmail.com>
2020-07-08 18:34:16 +0200

    add group show page with expenses belonging to the group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,10 @@
 class GroupsController < ApplicationController
-
   def index
     @groups = current_user.groups
+  end
+
+  def show
+    @group = Group.find(params[:id])
+    @expenses = @group.expenses
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'all_expenses', to: 'expenses#all_expenses'
   get 'all_external_expenses', to: 'expenses#all_external_expenses'
   get 'all_groups', to: 'groups#index'
+  resources :groups, only: [:show]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

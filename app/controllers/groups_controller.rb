9c245e3aeae0cfe7effcdbf58633fbb27c1c62f6
Azeem <azeemahmed101@gmail.com>
2020-07-09 15:05:25 +0200

    add delete action for groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -36,6 +36,12 @@ class GroupsController < ApplicationController
     end
   end
 
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    redirect_to all_groups_path
+  end
+
   private
 
   def group_params

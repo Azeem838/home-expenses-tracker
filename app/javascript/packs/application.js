1058230111d3b9d178bc4cda9caad2f8e3ae6cd8
Azeem <azeemahmed101@gmail.com>
2020-07-10 11:12:38 +0200

    add js for burger toggle

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,12 @@ require("channels");
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 require("jquery");
+
+$(document).ready(function () {
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function () {
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+  });
+});

0f82c8192b464ba27004b7c593625d45ee29fd2d
Azeem <azeemahmed101@gmail.com>
2020-07-11 16:15:35 +0200

    add active storage and display for group image

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -49,6 +49,6 @@ class GroupsController < ApplicationController
   private
 
   def group_params
-    params.require(:group).permit(:name)
+    params.require(:group).permit(:name, :main_image)
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -4,6 +4,19 @@ class Group < ApplicationRecord
   has_many :user_groups
   belongs_to :user
   validates :name, uniqueness: { case_sensitive: false }, presence: true
+  has_one_attached :main_image
+  validate :acceptable_image
 
   scope :total, ->(g) { g.expenses.sum(:amount) }
+
+  def acceptable_image
+    return unless main_image.attached?
+
+    errors.add(:main_image, 'is too big') unless main_image.byte_size <= 1.megabyte
+
+    acceptable_types = ["image/jpeg", "image/png"]
+    unless acceptable_types.include?(main_image.content_type)
+      errors.add(:main_image, "must be a JPEG or PNG")
+    end
+  end
 end

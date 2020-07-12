5b0a4dbc355034363511fcd787b3fd926ed5c169
Azeem <azeemahmed101@gmail.com>
2020-07-12 19:47:30 +0200

    add mail errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
-  
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'home-expenses-tracker.herokuapp.com', protocol: 'https' }
   # Full error reports are disabled and caching is turned on.

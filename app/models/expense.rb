b9670ae3f99b386fc75a0e993319da48993a1a53
Azeem <azeemahmed101@gmail.com>
2020-07-13 10:00:00 +0200

    add amount float validation

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -3,8 +3,9 @@ class Expense < ApplicationRecord
   has_many :expense_groups, dependent: :destroy
   has_many :groups, through: :expense_groups, dependent: :destroy
   validates :name, presence: true
-  validates :amount, presence: true
+  validates :amount, presence: true, numericality: {only_float: true}
   validates :author_id, presence: true
+
   scope :created_by, ->(user) { where(author_id: user.id) }
   scope :ungrouped, -> { where.not(id: ExpenseGroup.where(expense_id: ids).pluck(:expense_id)) }
   scope :grouped, -> { where(id: ExpenseGroup.where(expense_id: ids).pluck(:expense_id)) }

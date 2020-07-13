57a9f44396acbb9addd35ef003a5502697a37307
Azeem <azeemahmed101@gmail.com>
2020-07-13 09:00:11 +0200

    fix add and remove button bug and add external expenses link to users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,7 @@ class UsersController < ApplicationController
   def show
     @grand_total = current_user.expenses.sum(:amount)
     @groups = current_user.groups
+    @ungrouped = current_user.expenses.ungrouped
+    @total = Expense.sum_expenses(@ungrouped)
   end
 end

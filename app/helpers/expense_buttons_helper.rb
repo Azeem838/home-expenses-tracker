dca20b2954f8ef0f44e387a0a3867535f074154d
Azeem <azeemahmed101@gmail.com>
2020-07-14 08:59:11 +0200

    fix linter errors

--- app/helpers/expense_buttons_helper.rb
+++ app/helpers/expense_buttons_helper.rb
@@ -1,9 +1,12 @@
 module ExpenseButtonsHelper
   def show_buttons(obj, expense)
     if obj == @grouped
-      button_to 'Remove', expense_group_path(expense: expense.id, group: @group.id), method: :delete, data: { confirm: 'Are you sure?' }, class: 'button is-small is-block'
+      button_to 'Remove', expense_group_path(expense: expense.id, group: @group.id), method: :delete,
+                                                                                     data: { confirm: 'Are you sure?' },
+                                                                                     class: 'button is-small is-block'
     elsif obj == @ungrouped
-      button_to 'Add', expense_groups_path(expense: expense.id, group: @group.id), method: :post, class: 'button is-small is-block'
+      button_to 'Add', expense_groups_path(expense: expense.id, group: @group.id), method: :post,
+                                                                                   class: 'button is-small is-block'
     end
   end
 end
